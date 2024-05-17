a15734551976092b5c68af4dfc704b5423ba8ead
paintedblue <pinkclover22@naver.com>
2024-05-17 22:29:17 +0900

    design ; calendar ui

--- lib/screens/calendar/calendar_screen.dart
+++ lib/screens/calendar/calendar_screen.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
-import 'package:hci_03/constants/theme.dart';
-import 'package:hci_03/screens/components/custom_btn.dart'; // Import the CustomButton
+import 'package:hci_03/constants/theme.dart'; // textTheme import
 import '../../constants/image_assets.dart';
-import 'package:hci_03/screens/components/notice_box.dart';
-import 'package:hci_03/screens/components/custom_light_btn.dart';
 import '../components/appbar_preffered_size.dart';
-import 'package:hci_03/screens/battle/battle_screen.dart'; // Import the BattleScreen
 import 'package:hci_03/models/request_title.dart';
-
+import 'package:hci_03/screens/battle/components/promise_widget.dart'; // PromiseWidget import
+import 'package:hci_03/screens/calendar/dummy.dart'; // dummy.dart import
 
 class CalendarScreen extends StatefulWidget {
   const CalendarScreen({super.key});
@@ -37,43 +34,93 @@ class _CalendarScreenState extends State<CalendarScreen> {
           IconButton(
             icon: const Icon(Icons.settings),
             onPressed: () {
-              // Define your settings action here
+              // 설정 버튼 클릭 시 동작 정의
             },
           ),
         ],
-        bottom: appBarBottomLine(),
+        bottom: appBarBottomLine(), // AppBar 하단의 선
       ),
-      body: Column(
-        children: [
-          const SizedBox(height: 20), // 박스를 아래로 내리기 위해 추가
-          NoticeBox(notice: dummyNotices[4]),// Replace the notice box
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              child: TableCalendar(
-                firstDay: DateTime.utc(2010, 10, 16),
-                lastDay: DateTime.utc(2030, 3, 14),
-                focusedDay: _focusedDay,
-                selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
-                onDaySelected: (selectedDay, focusedDay) {
-                  setState(() {
-                    _selectedDay = selectedDay;
-                    _focusedDay = focusedDay;
-                  });
-                },
-                calendarFormat: CalendarFormat.month,
-                availableCalendarFormats: const {
-                  CalendarFormat.month: 'Month'
-                }, // Restricting to month view only
-                daysOfWeekStyle: DaysOfWeekStyle(
-                  weekdayStyle: TextStyle(fontSize: 12), // Adjust font size if necessary
-                  weekendStyle: TextStyle(fontSize: 12, color: Colors.red),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
+        child: Column(
+          children: [
+            const PromiseWidget(), // 상단에 PromiseWidget 추가
+            const SizedBox(
+              height: 15.0,
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: TableCalendar(
+                  firstDay: DateTime.utc(2010, 10, 16),
+                  lastDay: DateTime.utc(2030, 3, 14),
+                  focusedDay: _focusedDay,
+                  selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+                  onDaySelected: (selectedDay, focusedDay) {
+                    setState(() {
+                      _selectedDay = selectedDay;
+                      _focusedDay = focusedDay;
+                    });
+                  },
+                  calendarFormat: CalendarFormat.month, // 월간 보기로 제한
+                  availableCalendarFormats: const {
+                    CalendarFormat.month: 'Month'
+                  },
+                  daysOfWeekStyle: DaysOfWeekStyle(
+                    weekdayStyle: textTheme().bodySmall ?? TextStyle(fontSize: 12), // 평일 글꼴 스타일
+                    weekendStyle: textTheme().bodySmall?.copyWith(color: Colors.red) ?? TextStyle(fontSize: 12, color: Colors.red), // 주말 글꼴 스타일
+                  ),
+                  calendarBuilders: CalendarBuilders(
+                    defaultBuilder: (context, day, focusedDay) {
+                      if (day.isBefore(DateTime.now())) { // 과거 날짜 스타일
+                        return Container(
+                          margin: const EdgeInsets.all(4.0),
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: _getDayColor(day), // 날짜에 따른 색상 지정
+                            shape: BoxShape.circle,
+                          ),
+                          child: Text(
+                            '${day.day}',
+                            style: textTheme().bodySmall?.copyWith(color: Colors.black) ?? TextStyle(color: Colors.black), // 글씨색 검정색
+                          ),
+                        );
+                      } else if (isSameDay(day, DateTime.now())) { // 오늘 날짜 스타일
+                        return Container(
+                          margin: const EdgeInsets.all(4.0),
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black, width: 2),
+                            shape: BoxShape.circle,
+                          ),
+                          child: Text(
+                            '${day.day}',
+                            style: textTheme().bodySmall?.copyWith(color: Colors.black) ?? TextStyle(color: Colors.black), // 글씨색 검정색
+                          ),
+                        );
+                      }
+                      return null;
+                    },
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
+  }
+
+  // 날짜에 따라 색상을 반환하는 함수
+  Color _getDayColor(DateTime day) {
+    DateTime dateWithoutTime = DateTime.utc(day.year, day.month, day.day);
+    if (battleResults.containsKey(dateWithoutTime)) {
+      if (battleResults[dateWithoutTime] == 'A') {
+        return Color(0x80FFF73B); // 노란색, 50% 투명도
+      } else if (battleResults[dateWithoutTime] == 'B') {
+        return Color(0x804CD663); // 초록색, 50% 투명도
+      }
+    }
+    return Colors.grey.withOpacity(0.5); // 기본 색상, 50% 투명도
   }
 }
---
+++ lib/screens/calendar/dummy.dart
@@ -0,0 +1,22 @@
+// NOTE: calendar 에서 사용되는 컴포넌트 제작하는 폴더. 이 파일을 삭제해주세요// dummy.dart
+import 'package:flutter/material.dart';
+//날짜별 승리자 데이터
+final Map<DateTime, String> battleResults = {
+  DateTime.utc(2024, 5, 1): 'A',
+  DateTime.utc(2024, 5, 2): 'B',
+  DateTime.utc(2024, 5, 3): 'A',
+  DateTime.utc(2024, 5, 4): 'B',
+  DateTime.utc(2024, 5, 5): 'A',
+  DateTime.utc(2024, 5, 6): 'A',
+  DateTime.utc(2024, 5, 7): 'A',
+  DateTime.utc(2024, 5, 8): 'B',
+  DateTime.utc(2024, 5, 9): 'A',
+  DateTime.utc(2024, 5, 10): 'B',
+  DateTime.utc(2024, 5, 11): 'B',
+  DateTime.utc(2024, 5, 12): 'B',
+  DateTime.utc(2024, 5, 13): 'B',
+  DateTime.utc(2024, 5, 14): 'A',
+  DateTime.utc(2024, 5, 15): 'A',
+  DateTime.utc(2024, 5, 16): 'A',
+  // 다른 날짜와 결과를 여기에 추가하세요.
+};
